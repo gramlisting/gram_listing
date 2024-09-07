f7ec1c06a7f4205c58a873f6392f07dcf952e720
Andrew <andrew.sideby@gmail.com>
2024-09-07 21:55:49 +0800

    --destructive RGB 255 97 84

--- apps/website/src/components/meteors-card.tsx
+++ apps/website/src/components/meteors-card.tsx
@@ -1,12 +1,11 @@
-
 import type { Meteor } from "@/types/meteors";
-import {Meteors} from "@/components/uix/meteors";
+import { Meteors } from "@/components/uix/meteors";
 
 export function MeteorsCard({ meteor }: { meteor: Meteor }) {
   return (
     <div className="w-full">
       <div className="relative w-full">
-        <div className="absolute inset-0 h-full w-full scale-[0.80] transform rounded-full bg-red-500 bg-gradient-to-r from-blue-500 to-teal-500 blur-3xl" />
+        <div className="absolute inset-0 h-full w-full scale-[0.90] transform rounded-full bg-red-500 bg-gradient-to-r from-pink-300 to-yellow-200 dark:from-pink-900 dark:to-yellow-900 blur-3xl" />
         <div className="relative flex h-full flex-col items-start justify-end overflow-hidden rounded-2xl border border-gray-800 px-4 py-4 shadow-xl dark:bg-gray-900 dark:bg-opacity-70">
           <h1 className="relative z-50 mb-4 text-2xl font-bold">
             {meteor.name}
@@ -21,7 +20,7 @@ export function MeteorsCard({ meteor }: { meteor: Meteor }) {
             </button>
 
             {/* Meaty part - Meteor effect */}
-            <Meteors number={20} />
+            {/*<Meteors number={20} />*/}
           </a>
         </div>
       </div>
