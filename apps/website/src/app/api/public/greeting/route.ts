5942bf3396e7d637c8d92f1d29c2193b256680bd
Andrew <andrew.sideby@gmail.com>
2024-09-10 09:41:36 +0800

    brand update

--- apps/website/src/app/api/public/greeting/route.ts
+++ apps/website/src/app/api/public/greeting/route.ts
@@ -1,4 +1,6 @@
 import type { NextRequest, NextResponse } from "next/server";
+import { db } from "@gramlisting/db";
+import superjson from "superjson";
 //
 // export async function GET(request: NextRequest) {
 //   try {
@@ -11,7 +13,13 @@ import type { NextRequest, NextResponse } from "next/server";
 
 const handler = async (req: NextRequest) => {
   try {
-    return Response.json({ api: "get /api/public/greeting" });
+    let prismaPromise = await db.user.findMany();
+    let data = superjson.stringify(prismaPromise);
+    console.info(data);
+    return Response.json({
+      api: "get /api/public/greeting",
+      jsonData: data,
+    });
   } catch (error) {
     console.info(error);
     return Response.json(error);
